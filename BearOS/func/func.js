f913570155ffdacca9164597e3397a75df6aaafc
Bearrwin <125781212+Bearrwin@users.noreply.github.com>
2026-03-09 11:03:03 +1100

    Add files via upload

--- BearOS/func/func.js
+++ BearOS/func/func.js
@@ -1,53 +1,11 @@
 /** @param {NS} ns */
 
+
+
 export function cloudMaxSize(ns, mult) {
-
-	let maxSize = Math.ceil(mult * 1048576)
-
-		switch (true) {
-
-		case maxSize < 4:
-			return 2
-		case maxSize < 8:
-			return 4
-		case maxSize < 16:
-			return 8
-		case maxSize < 32:
-			return 16
-		case maxSize < 64:
-			return 32
-		case maxSize < 128:
-			return 64
-		case maxSize < 256:
-			return 128
-		case maxSize < 512:
-			return 256
-		case maxSize < 1024:
-			return 512
-		case maxSize < 2048:
-			return 1024
-		case maxSize < 4096:
-			return 2048
-		case maxSize < 8192:
-			return 4096
-		case maxSize < 16384:
-			return 8192
-		case maxSize < 32768:
-			return 16384
-		case maxSize < 65536:
-			return 32768
-		case maxSize < 131072:
-			return 65536
-		case maxSize < 262144:
-			return 131072
-		case maxSize < 524288:
-			return 262144
-		case maxSize < 1048576:
-			return 524288
-
-		}
-
+  return 2 ** (0|Math.log2(mult * 1048576));
 }
+
 export function threadMax(ns, host, scriptSize) {
 	let threadNum = 1;
 	let freeRam = ns.getServerMaxRam(host) - ns.getServerUsedRam(host);
